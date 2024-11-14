bc2f63841931c0158ac33c1e55ced683049a6fe5
santino <santinocasagerone@gmail.com>
2024-11-14 15:48:13 -0300

    anteultima version

---
+++ sql/insertar.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO usuario (nombre, apellido, tipoDocumento, nroDocumento, tipoUsuario, curso_id) VALUES ('Juan', 'Pérez', 'DNI', 46236721, 'Alumno', 1);
+INSERT INTO curso (anio, division, id_c) VALUES (1, 2, 1);
