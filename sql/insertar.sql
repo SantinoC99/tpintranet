c8ffe076f86fcc22c661133580b66cd44f170bb0
pabxer <135927357+pabxer@users.noreply.github.com>
2024-11-15 19:37:31 -0300

    Delete sql directory

--- sql/insertar.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO usuario (nombre, apellido, tipoDocumento, nroDocumento, tipoUsuario, curso_id) VALUES ('Juan', 'Pérez', 'DNI', 46236721, 'Alumno', 1);
-INSERT INTO curso (anio, division, id_c) VALUES (1, 2, 1);
